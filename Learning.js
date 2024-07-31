f1fdd6b930ec6e273aef734fbda01485393ababa
Elnior <elniorloreh@gmail.com>
2024-07-30 22:35:42 -0400

    El servidor esta complementado para crear secciones de aprendizaje, ahora se debe de seguir para completar dicha funcionalidad hasta tal punto de auto escalar.

--- Learning.js
+++ Learning.js
@@ -1,11 +1,26 @@
 import fs from 'node:fs';
 import net from 'node:net';
 import anully from 'anully';
+import http from 'node:http';
 import { setTimeout as delay } from 'node:timers/promises';
 
 async function isSource (name, dir) {
 	let files = await fs.promises.readdir(dir);
 	return files.indexOf(name) !== -1;
+}
+
+function isAdyacent (path) {
+	return new Promise ((resolve, reject)=>
+		fs.exists("./interactivity", resolve)
+	);
+}
+
+function getSection (name) {
+	return {
+		name,
+		birthTime: Date.now(),
+		reference: name.replaceAll(" ", "-")
+	}
 }
 
 async function dispatchSocket (obR, socketActual) {
@@ -36,6 +51,39 @@ async function dispatchSocket (obR, socketActual) {
 							"\r\n"
 						);
 						socketActual.write("there are internal errors..");
+						callEnd();
+					})
+					.on("end", callEnd)
+					.on("close", callEnd);
+				});
+			}
+			else if (obR.path == "/" && obR.headers.accept.indexOf("application/json") == 0) {
+
+				const sectionStream = fs.createReadStream("./interactivity/sections.json");
+
+				await new Promise((resolve, reject)=> {
+					let isRun = false;
+
+					const callEnd = ()=> !isRun? resolve(isRun = true) : null;
+
+					sectionStream
+					.on("open", ()=> socketActual.write(
+							"HTTP/1.1 200 ok\r\n" +
+							"content-type: application/json\r\n" +
+							"connection: keep-alive\r\n" +
+							"server: NElniorS\r\n" +
+							"\r\n"
+						))
+					.on("data", chunk=> socketActual.write(chunk))
+					.on("error", error=> {
+						socketActual.write(
+						"HTTP/1.1 419 there are no\r\n" +
+						"content-type: text/txt\r\n" +
+						"connection: close\r\n" +
+						"server: NElniorS\r\n" +
+						"\r\n"
+					);
+					socketActual.write("There are no sections.. Create a new section (+)");
 						callEnd();
 					})
 					.on("end", callEnd)
@@ -148,6 +196,201 @@ async function dispatchSocket (obR, socketActual) {
 				socketActual.write(`<h1 style="color: red; text-align:center">Not found direction: ${obR.path}</h1>`);
 			}
 		break;
+
+		case 'post' :
+			if (obR.path == "/create/section" && obR.headers['content-type'] == "text/txt") {
+				const condition = /^[a-z]{3,}([\s]|[a-z])*$/i;
+				if ( condition.test(obR.body.trim()) ) {
+					try {
+						let dir = "./interactivity";
+
+						if (await isAdyacent(dir)) {
+							if (await isAdyacent(`${dir}/sections.json`)) {
+								// I'm set new section
+								let sectionsReadableStream = fs.createReadStream(`${dir}/sections.json`);
+								let value = obR.body.trim(),
+									isEqualToBeforeName = false;
+
+								let beFromHere = fs.createWriteStream("./fromHere.json");
+								beFromHere.write("[\r\n");
+								let stringForObject = "";
+
+								let isOpen = false;
+								let isClose = false;
+								await new Promise ((resolve, reject)=> {
+									let isRun = false;
+									const callEnd = ()=> !isRun? resolve(isRun = true) : null;
+
+									sectionsReadableStream.on("data", chunk=> {
+										for (let each of chunk.toString('utf-8')) {
+											// verifing
+											if (each == "{")
+												isOpen = true;
+											else if (each == "}")
+												isClose = true;
+											else {}
+
+											if (isOpen)
+												stringForObject += each;
+											else {}
+
+											if (isClose) {
+												// for each object
+												let forEvaluate = JSON.parse(stringForObject);
+												if (forEvaluate.name == value) {
+													isEqualToBeforeName = true;
+													sectionsReadableStream.destroy();
+													break;
+												}
+
+												beFromHere.write(stringForObject + ",\r\n");
+												// reset values
+												isOpen = false;
+												isClose = false;
+												stringForObject = "";
+											}
+										}
+									})
+									.on("error", reject)
+									.on("end", ()=> {
+										if (isEqualToBeforeName) return;
+										
+										beFromHere.write( JSON.stringify(getSection(value)) + "\r\n" );
+										beFromHere.write("]");
+										beFromHere.end();
+
+									})
+									.on("close", ()=> {
+										if (isEqualToBeforeName) {
+											socketActual.write(
+												"HTTP/1.1 241 there are this\r\n" +
+												"content-type: text/txt\r\n" +
+												"connection: close\r\n" +
+												"Server: NElniorS\r\n" +
+												`Date: ${new Date()}\r\n` +
+												"\r\n"
+											);
+											socketActual.write("This name already exists, please write another one.");
+											callEnd();
+										}
+										else {
+											beFromHere = fs.createReadStream(beFromHere.path);
+											let sectionsWritableStream = fs.createWriteStream(sectionsReadableStream.path);
+
+											sectionsWritableStream
+											.on("close", callEnd)
+											.on("error", reject);
+
+											beFromHere.pipe(sectionsWritableStream);
+
+											socketActual.write(
+												"HTTP/1.1 206 Created\r\n" +
+												"content-type: text/txt\r\n" +
+												"connection: close\r\n" +
+												"Server: NElniorS\r\n" +
+												`Date: ${new Date()}\r\n` +
+												"\r\n"
+											);
+											socketActual.write("created!");
+										}
+									});
+								});
+							}
+							else {
+								// I'm create the file
+								let sectionsStream = fs.createWriteStream(`${dir}/sections.json`);
+
+								await new Promise ((resolve, reject)=> {
+									sectionsStream.on("finish", ()=> {
+										socketActual.write(
+											"HTTP/1.1 206 Created\r\n" +
+											"content-type: text/txt\r\n" +
+											"connection: close\r\n" +
+											"Server: NElniorS\r\n" +
+											`Date: ${new Date()}\r\n` +
+											"\r\n"
+										);
+										socketActual.write("created!");
+										resolve();
+									});
+
+									sectionsStream.write("[\r\n");
+
+									sectionsStream.write(JSON.stringify(getSection(obR.body.trim())) + "\r\n");
+
+									sectionsStream.write("]");
+
+									sectionsStream.end();
+								});
+							}
+						}
+						else {
+							// I'm creating directory..
+							await fs.promises.mkdir(dir);
+							// I'm create the file
+							let sectionsStream = fs.createWriteStream(`${dir}/sections.json`);
+
+							await new Promise ((resolve, reject)=> {
+								sectionsStream.on("finish", ()=> {
+									socketActual.write(
+										"HTTP/1.1 206 Created\r\n" +
+										"content-type: text/txt\r\n" +
+										"connection: close\r\n" +
+										"Server: NElniorS\r\n" +
+										`Date: ${new Date()}\r\n` +
+										"\r\n"
+									);
+									socketActual.write("created!");
+									resolve();
+								});
+
+								sectionsStream.write("[\r\n");
+
+								sectionsStream.write(JSON.stringify(getSection(obR.body.trim())) + "\r\n");
+
+								sectionsStream.write("]");
+
+								sectionsStream.end();
+							});
+						}
+					}
+					catch (error) {
+						socketActual.write(
+							"HTTP/1.1 211 Expected\r\n" +
+							"content-type: text/txt\r\n" +
+							"connection: close\r\n" +
+							"Server: NElniorS\r\n" +
+							`Date: ${new Date()}\r\n` +
+							"\r\n"
+						);
+						socketActual.write("Falied to create section!");
+					}
+				}
+				else {
+					socketActual.write(
+						"HTTP/1.1 230 invalid name\r\n" +
+						"content-type: text/txt\r\n" +
+						"connection: keep-alive\r\n" +
+						"Server: NElniorS\r\n" +
+						`Date: ${new Date()}\r\n` +
+						"\r\n"
+					);
+					socketActual.write(`the name "${obR.body.trim()}" is not acceptable, numbers and symbols is not acceptable; write again.`);
+				}
+			}
+			else {
+				socketActual.write(
+					"HTTP/1.1 400 Bad Request\r\n" +
+					"content-type: text/html; charset=utf-8\r\n" +
+					"connection: close\r\n" +
+					"Server: NElniorS\r\n" +
+					`Date: ${new Date()}\r\n` +
+					"\r\n"
+				);
+				socketActual.write(`<h1 style="color: red; text-align:center">Bad Request, the request is not valid.</h1>`);
+			}
+		break;
+
 		default :
 			socketActual.write(
 				"HTTP/1.1 409 there are no method\r\n" +
--- js/toLearn.js
+++ js/toLearn.js
@@ -6,30 +6,151 @@ function delay (time) {
 }
 
 class HandlerSection {
+	#processing = false;
+	#IsPresent = false;
+	#listOfSections = null;
 	constructor (loader, $nav) {
 		this.loader = loader;
 		this.$nav = $nav;
 		// the Nodes for documents
 		this.thereAreNoNode = document.createElement("div");
 		this.thereAreNoNode.className = "thereAreNo";
-		this.thereAreNoNode.textContent = "There are no sections.. create a new section (+)";
 	}
-	getFragmentOfDocument (parsedSections) {
+	getFragmentOfDocument (sections) {
 		const fragment = document.createDocumentFragment();
-		let $ul = document.createElement("ul"),
-		$li = document.createElement("li"),
-		$a = document.createElement("a"),
-		$button = document.createElement("button");
-		$button.textContent = "...";
-		$button.className = "moreActions";
 
-		//...
+		for (let section of sections) {
+			// each elements collection
+			let	$button = document.createElement("button");
+			$button.textContent = "...";
+			$button.className = "moreActions";
+
+			let $li = document.createElement("li"),
+				$a = document.createElement("a");
+			$a.href = `/${section.replaceAll(" ", "-")}`;
+			$a.textContent = section;
+			$li.appendChild($a);
+			$li.appendChild($button);
+			fragment.appendChild($li);
+		}
 
 		return fragment;
 	}
+	async loadSections() {
+		try {
+			const response = await fetch(window.location.href, {
+				method: 'get',
+				headers: {
+					accept: "application/json",
+					date: new Date()
+				},
+				mode: 'cors',
+				body: null
+			});
+			if (response.ok) {
+				let sections = this.#listOfSections = await response.json();
+
+				let $ul = document.createElement("ul");
+						$ul.id = "section-list";
+
+					let $list = this.getFragmentOfDocument( sections.map(object=> object.name) );
+					$ul.appendChild($list);
+						
+					this.$nav.appendChild($ul);
+			}
+			else {
+				this.thereAreNoNode.textContent = await response.text();
+				this.$nav.appendChild(this.thereAreNoNode);
+			}
+		}
+		catch (error) {
+			this.thereAreNoNode.textContent = error.message;
+			this.$nav.appendChild(this.thereAreNoNode);
+		}
+		finally {
+			this.loader.parentElement.removeChild(this.loader);
+		}
+	}
+	async handlerEventSubmit (evArg) {
+		if (evArg.target.matches("form#creatingSection")) {
+			evArg.preventDefault();
+			const obj = {
+				method: evArg.target.method,
+				headers: {
+					"content-type": "text/txt",
+					"accept": "text/txt",
+					"date": new Date()
+				},
+				body: evArg.target.section.value
+			},
+			{ action } = evArg.target;
+			const $message = evArg.target.querySelector("div.message");
+
+			try {
+				let response = await fetch(action, obj);
+				if(response.status == 206) {
+					let $ul = this.$nav.querySelector("ul#section-list");
+					if ($ul) {
+						let $list = this.getFragmentOfDocument([obj.body]);
+						$ul.appendChild($list);
+						
+						this.$nav.appendChild($ul);
+					}
+					else {
+						$ul = document.createElement("ul");
+						$ul.id = "section-list";
+
+						let $list = this.getFragmentOfDocument([obj.body]);
+						$ul.appendChild($list);
+						
+						this.$nav.appendChild($ul);
+					}
+					// Remove boxs
+					let $div = this.$nav.querySelector("div.thereAreNo");
+
+					if ($div)
+						this.$nav.removeChild($div);
+
+					const $sectionToDelete = document.querySelector("section#createSection");
+					document.body.removeChild($sectionToDelete);
+					this.#processing = false;
+				}
+				else {
+					$message.textContent = await response.text();
+					await delay(3500);
+					$message.textContent = '';
+				}
+			}
+			catch (error) {
+				$message.textContent = error.message;
+				await delay(3500);
+				$message.textContent = '';
+			}
+		}
+	}
+	handlerEventClick (evArg) {
+		if (evArg.target.matches("button#set-new-section") && !this.#processing) {
+			this.#processing = true;
+			let $generateSection = document.getElementById("@generateSection");
+			let imported = document.importNode($generateSection.content, true);
+			document.body.appendChild(imported);
+		}
+		else if (evArg.target.matches("div.controls input#cancel")) {
+			const $sectionToDelete = document.querySelector("section#createSection");
+			document.body.removeChild($sectionToDelete);
+			this.#processing = false;
+		}
+	}
 	static async Main () {
 		const handling = new HandlerSection( document.getElementById("loader"), document.getElementById("all-sections") );
-		// handling.loadSections();
+		await delay (4000);
+		await handling.loadSections();
+
+		// The click event Handler..
+		document.addEventListener("click", handling.handlerEventClick.bind(handling));
+
+		//The submit event Handler..
+		document.addEventListener("submit", handling.handlerEventSubmit.bind(handling));
 	}
 }
 
